e7095749e04bf8c4f88317f7ffe2285d87e07875
Samuel Leonel <samueleonelfreitas@hotmail.com>
2021-05-11 17:21:51 -0300

    components/ModalEditFood.jsx to components/ModalEditFood.tsx

---
+++ src/components/ModalEditFood/index.tsx
@@ -0,0 +1,55 @@
+import { Component, createRef, useRef } from 'react';
+import { FiCheckSquare } from 'react-icons/fi';
+
+import { Form } from './styles';
+import Modal from '../Modal';
+import Input from '../Input';
+import { FormHandles } from '@unform/core';
+
+interface food {
+  id: number,
+  name: string,
+  description: string,
+  price: string,
+  available: boolean,
+  image: string
+}
+
+interface propsData {
+  setIsOpen: () => void;
+  handleUpdateFood: (food: food) => void;
+  isOpen: boolean
+  editingFood: food;
+}
+
+const ModalEditFood = ({setIsOpen, handleUpdateFood, isOpen, editingFood}: propsData) => {
+  const formRef = useRef<FormHandles>(null)
+
+  const handleSubmit = async (data: food) => {
+    handleUpdateFood(data);
+    setIsOpen();
+  };
+
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
+        <Input name="image" placeholder="Cole o link aqui" />
+
+        <Input name="name" placeholder="Ex: Moda Italiana" />
+        <Input name="price" placeholder="Ex: 19.90" />
+
+        <Input name="description" placeholder="Descrição" />
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
+          <div className="icon">
+            <FiCheckSquare size={24} />
+          </div>
+        </button>
+      </Form>
+    </Modal>
+  );
+}
+
+export default ModalEditFood;
